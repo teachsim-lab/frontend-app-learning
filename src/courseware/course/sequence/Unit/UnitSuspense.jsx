9ed241f0323248f42aa6554b0db7512945067ff0
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2026-04-01 16:07:36 +0500

    fix: unit prerequisite within same subsection
    Learners had to refresh the page if they complete pre-req unit within same subsection due to upstream bug as it doesn't fetch unit completion automatically and user has to refresh the page.

--- src/courseware/course/sequence/Unit/UnitSuspense.jsx
+++ src/courseware/course/sequence/Unit/UnitSuspense.jsx
@@ -1,11 +1,14 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 
 import { useIntl } from '@edx/frontend-platform/i18n';
+import { useDispatch } from 'react-redux';
 
 import { useModel } from '@src/generic/model-store';
 import PageLoading from '@src/generic/PageLoading';
 import { GatedUnitContentMessageSlot } from '../../../../plugin-slots/GatedUnitContentMessageSlot';
+import { checkBlockCompletion, fetchSequence } from '../../../data';
 
 import messages from '../messages';
 import ContentLock from '../content-lock';
@@ -18,6 +21,8 @@ const UnitSuspense = ({
   id,
 }) => {
   const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
   const shouldDisplayHonorCode = hooks.useShouldDisplayHonorCode({ courseId, id });
   const unit = useModel(modelKeys.units, id);
   const meta = useModel(modelKeys.coursewareMeta, courseId);
@@ -27,6 +32,36 @@ const UnitSuspense = ({
 
   // Check for unit-level prerequisites
   const isUnitGated = unit && unit.isGated;
+
+  useEffect(() => {
+    const prereqId = unit?.gatedContent?.prereqId;
+    const sequenceId = unit?.sequenceId;
+    if (!courseId || !sequenceId || !prereqId || !isUnitGated) {
+      return undefined;
+    }
+
+    const isPreview = pathname.startsWith('/preview');
+
+    let cancelled = false;
+
+    const refreshIfUnlocked = async () => {
+      const prereqIsComplete = await dispatch(checkBlockCompletion(courseId, sequenceId, prereqId));
+      if (cancelled) {
+        return;
+      }
+
+      if (prereqIsComplete) {
+        dispatch(fetchSequence(sequenceId, isPreview));
+      }
+    };
+
+    refreshIfUnlocked();
+    const intervalId = setInterval(refreshIfUnlocked, 5000);
+    return () => {
+      cancelled = true;
+      clearInterval(intervalId);
+    };
+  }, [courseId, dispatch, isUnitGated, pathname, unit?.gatedContent?.prereqId, unit?.sequenceId]);
 
   return (
     <>
